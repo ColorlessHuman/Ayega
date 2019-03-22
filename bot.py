c503c2f015f885dfefc95360bdb16ece5742f051
Jyotirmoy Mandal <jyotirmoy@getvymo.com>
2019-03-22 13:24:56 +0530

    added ping command

--- bot.py
+++ bot.py
@@ -18,17 +18,21 @@ client = commands.Bot(command_prefix='?')
 async def on_ready():
     print('Apna Time Ayega!')
 
-@client.event
-async def on_message(message):
-    author = message.author
-    content = message.content
-    print('{}: {}'.format(author, content))
+# @client.event
+# async def on_message(message):
+#     author = message.author
+#     content = message.content
+#     print('{}: {}'.format(author, content))
+#
+# @client.event
+# async def on_message_delete(message):
+#     author = message.author
+#     content = message.content
+#     channel = message.channel
+#     await client.send_message(channel, '{}: {}'.format(author, content))
 
-@client.event
-async def on_message_delete(message):
-    author = message.author
-    content = message.content
-    channel = message.channel
-    await client.send_message(channel, '{}: {}'.format(author, content))
+@client.command()
+async def ping():
+    await client.say('Pong!')
 
 client.run(TOKEN)
