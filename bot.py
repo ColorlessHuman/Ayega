c17c415a6616ed435de17d510c00f41cc15e4dd6
Jyotirmoy Mandal <jyotirmoy@getvymo.com>
2019-03-22 13:39:12 +0530

    added logging config

--- bot.py
+++ bot.py
@@ -6,8 +6,16 @@ A Discord Bot
 """
 
 import discord
+import logging
 import os
 from discord.ext import commands
+
+# Log config
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 TOKEN = os.environ['AYEGA_TOKEN']
@@ -42,5 +50,6 @@ async def echo(*kwargs):
         output += word
         output += ' '
     await client.say(output)
+
 
 client.run(TOKEN)
