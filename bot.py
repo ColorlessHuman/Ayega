238c9a298eb1ecb81652288ffa7f01f40024ceaf
Jyotirmoy Mandal <jyotirmoy@getvymo.com>
2019-03-22 12:48:39 +0530

    Basic bot setup

--- bot.py
+++ bot.py
@@ -4,5 +4,18 @@ by Jyotirmoy Mandal
 
 A Discord Bot
 """
+
 import discord
+import os
 from discord.ext import commands
+
+
+TOKEN = os.environ['AYEGA_TOKEN']
+
+client = commands.Bot(command_prefix='?')
+
+@client.event
+async def on_ready():
+    print('Apna Time Ayega!')
+
+client.run(TOKEN)
